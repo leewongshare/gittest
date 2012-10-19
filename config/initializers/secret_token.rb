32735761c8075cb34d17d4d025745cb6d1ae13d9
wong lee <write.wonglee@gmail.com>
2012-10-19 10:26:20 -0400

    gittest project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gittest::Application.config.secret_token = 'f6a3964044bce7aae2c8390b85a908790b03ee75d0ec693bd9f5a3112df6dc04daab3e974ea05c71b49fd1626e0b78e66eb6afc06d3dafe13165eb78a76fbea5'
